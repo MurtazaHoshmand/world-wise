ec6989fe6310f1f6515984d09356abe6fbde1d54
MurtazaHoshmand <145467980+MurtazaHoshmand@users.noreply.github.com>
2024-11-18 07:44:25 +0430

    cities area deed

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,47 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
 
+const BASE_URL = "http://localhost:9000"
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try{
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      }catch{
+        console.log("error")
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities();
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
+
+        <Route path="/app" element={<AppLayout />} >
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          <Route path="countries"  element={<p>countries</p>}/>
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          <Route path="form" element={<p>form</p>}/>
+        </Route>
+
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         {/* <Route path="/" element={<Home/>}/> */}
       </Routes>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,15 @@
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
-    return (
-        <div>
-            AppLayout
-        </div>
-    )
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
